e0113fcbb0065aa623bb21505323aa09c9960cf9
Harsh Raj <harshraj9142@gmail.com>
2025-11-24 15:31:20 +0530

    add theme context with dark mode support and refactor tab layout colors

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity} from "react-native";
+import useTheme from "@/hooks/useTheme";
 
 export default function Index() {
+  
+  const {toggleDarkMode} = useTheme();
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.content}>This is the Todos Tab</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
+
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+  content:{
+    fontSize:22
+  }
+})
