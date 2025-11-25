952512c236df045d05adfee640849c58dd3307fc
Harsh Raj <harshraj9142@gmail.com>
2025-11-25 19:26:01 +0530

    enhance Todos tab with gradient background and dark mode support; add home and settings styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,30 @@
-import { StyleSheet, Text, View, TouchableOpacity} from "react-native";
+import {StatusBar, Text,TouchableOpacity } from "react-native";
 import useTheme from "@/hooks/useTheme";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
+
+  const { toggleDarkMode, colors} = useTheme();
   
-  const {toggleDarkMode} = useTheme();
-  
-  return (
-    <View style={styles.container}>
-      <Text style={styles.content}>This is the Todos Tab</Text>
+  const homeStyles = createHomeStyles(colors);
+
+  return(
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+    
+    <StatusBar barStyle={colors.statusBarStyle}/>
+    <SafeAreaView style={homeStyles.safeArea}>
+      <Text>This is the Todos Tab</Text>
+
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Click Me</Text>
       </TouchableOpacity>
 
-
-    </View>
+    
+    </SafeAreaView>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 10,
-  },
-  content:{
-    fontSize:22
-  }
-})
+
