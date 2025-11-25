5eda0ddb000b139bee21e12b5cbe0dc87b5046e7
Harsh Raj <harshraj9142@gmail.com>
2025-11-25 20:31:20 +0530

    fintegrate TodoInput and Header components; fetch todos data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,10 @@ import useTheme from "@/hooks/useTheme";
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
+import Header from "@/components/Header";
+import TodoInput from "@/components/TodoInput";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Index() {
 
@@ -10,12 +14,17 @@ export default function Index() {
   
   const homeStyles = createHomeStyles(colors);
 
+  const todos = useQuery(api.todos.getTodos);
+
+  if (isLoading) return <LoadingSpinner />;
+
   return(
     <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
     
     <StatusBar barStyle={colors.statusBarStyle}/>
     <SafeAreaView style={homeStyles.safeArea}>
-      <Text>This is the Todos Tab</Text>
+      <Header/>
+      <TodoInput/>
 
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Click Me</Text>
